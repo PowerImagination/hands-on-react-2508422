0615f6a72639297acfc1bb77c21ab32fdd9b00d1
Hicham <ezzhic@gmail.com>
2024-08-26 15:16:11 +0000

    container +article+button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 function App() {
   return (
-    <hgroup>
+    <div className="container">
+    <article>
+      <img src="images/group.svg"/>
+       <hgroup>
       <h1>Meet the StarGazers</h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <button className="outline" onClick={()=> alert('Hello World')}>Click Me</button>
     </hgroup>
+    </article>
+     </div>
   )
 }
 export default App
