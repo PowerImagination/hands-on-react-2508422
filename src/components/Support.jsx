4587edee731ea15050db1c182d4de11c083d1900
Hicham <ezzhic@gmail.com>
2024-08-27 14:17:03 +0000

    Using effects

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+const pageTitle = document.title;
 export default () => {
   const [count, setCount] = useState(0)
+  useEffec(() => {
+   count && (document.title = `${pageTitle} -- ${count}`)
+  })
   return (
     <button className="outline" onClick={() => setCount(count + 1)}>
       {count === 0 ? "Click to support": `Supported ${count} times`}
